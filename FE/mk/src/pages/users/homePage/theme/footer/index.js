e8f0e949a33c43b4d4a623dcdba0cf4daca08fbc
Thao My <myvu2411@gmail.com>
2023-12-06 11:14:09 +0700

    Footer

--- FE/mk/src/pages/users/homePage/theme/footer/index.js
+++ FE/mk/src/pages/users/homePage/theme/footer/index.js
@@ -1,7 +1,77 @@
 import { memo } from "react";
+import './style.scss';
+import { Link } from "react-router-dom";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillPhone } from "react-icons/ai";
 
 const Footer = () => {
-    return <h1>Footer</h1>;
+    return <footer className="footer">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3">
+                    <div className="footer__about">
+                        <h1 className="footer__about_logo">MK SHOP</h1>
+                        <ul>
+                            <li>Address: 304 Nguyen Duy</li>
+                            <li>Phone: 0778-763-935</li>
+                            <li>Email: mk1610@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="footer__widget">
+                        <h6>Stores</h6>
+                        <ul>
+                            <li>
+                                <Link to="">Contact</Link>
+                            </li>
+                            <li>
+                                <Link to="">Information about us</Link>
+                            </li>
+                            <li>
+                                <Link to="">Bussiness Products</Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <Link to="">Account Information</Link>
+                            </li>
+                            <li>
+                                <Link to="">Cart</Link>
+                            </li>
+                            <li>
+                                <Link to="">Favorites List</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <div className="footer__widget">
+                        <h6>Promotion & Concessionary</h6>
+                        <p>Sign up to recieve Information here</p>
+                        <form action="#">
+                            <div>
+                                <input type="text" placeholder="Enter Email" />
+                                <button type="submit" className="button-submit">Sign Up</button>
+                            </div>
+                            <div className="footer__widget-social">
+                                <div>
+                                    <AiFillFacebook />
+                                </div>
+                                <div>
+                                    <AiFillInstagram />
+                                </div>
+                                <div>
+                                    <AiFillPhone />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>;
 }
 
 export default memo(Footer);
